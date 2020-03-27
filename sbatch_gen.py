06870614a82721b5ba81ba39a23258112bde44c0
Jia Liu <jialiu@berkeley.edu>
2020-03-27 11:29:21 -0700

    do multiple redshifts at a time

--- sbatch_gen.py
+++ sbatch_gen.py
@@ -9,7 +9,7 @@ def write_sbatch(zs):
 #SBATCH --tasks-per-node=32
 #SBATCH --cpus-per-task=1
 #SBATCH -C haswell
-#SBATCH -t 1:00:00
+#SBATCH -t 4:00:00
 #SBATCH -J run-wlen-8192-5000-zs{0}
 #SBATCH -o zs.{0}_4096-5000.o%j
 #SBATCH -e zs.{0}_4096-5000.e%j
@@ -28,16 +28,20 @@ bcast-pip mpsort
 
 SRC=/global/cscratch1/sd/akrolew/8192-5000/lightcone/usmesh/
 DST=/global/cscratch1/sd/jialiu/fastpm/CorrelatedSims/wlen_jliu
-BIN=/global/cscratch1/sd/jialiu/fastpm/CorrelatedSims/wlen_jliu_1zs.py
-for zs in `seq {0} 0.1 2.2`
-do echo $zs
-srun python -u $BIN $DST $SRC $zs --zlmin 0.0 --zlmax 2.2 --zstep=0.02 --nside=4096 &
-wait
-done
+BIN=/global/cscratch1/sd/jialiu/fastpm/CorrelatedSims/wlen_jliu_multiple.py
+
+srun python -u $BIN $DST $SRC $zs --zlmin 0.0 --zlmax 2.2 --zstep=0.2 --nside=4096
+
+#BIN=/global/cscratch1/sd/jialiu/fastpm/CorrelatedSims/wlen_jliu_1zs.py
+#for zs in `seq {0} 0.1 2.2`
+#do echo $zs
+#srun python -u $BIN $DST $SRC $zs --zlmin 0.0 --zlmax 2.2 --zstep=0.02 --nside=4096 &
+#wait
+#done
 '''.format(zs)
     f.write(content)
 
-for zs in np.arange(0.1, 0.2, 0.01):
+for zs in np.arange(1.2, 1.4, 0.01):#(0.1, 0.2, 0.01):
     write_sbatch(zs)
 
 
