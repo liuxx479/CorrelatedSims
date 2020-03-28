a5059f257391aff5af3542f4f8ab59f3ed2bff01
Jia Liu <jialiu@berkeley.edu>
2020-03-28 11:15:06 -0700

    generate files by batch

--- sbatch_gen.py
+++ sbatch_gen.py
@@ -3,13 +3,17 @@
 import numpy as np
 
 def write_sbatch(zs):
+    zs_list = np.arange(zs, 2.2, 0.1)
+    zs_str=''
+    for izs in zs_list:
+        zs_str+='%.2f '%(izs)
     f = open('jobs/sbatch_zs{0}'.format(zs), 'w')
     content='''#! /bin/bash
 #SBATCH --nodes=32
 #SBATCH --tasks-per-node=32
 #SBATCH --cpus-per-task=1
 #SBATCH -C haswell
-#SBATCH -t 4:00:00
+#SBATCH -t 2:00:00
 #SBATCH -J run-wlen-8192-5000-zs{0}
 #SBATCH -o zs.{0}_4096-5000.o%j
 #SBATCH -e zs.{0}_4096-5000.e%j
@@ -30,6 +34,7 @@ SRC=/global/cscratch1/sd/akrolew/8192-5000/lightcone/usmesh/
 DST=/global/cscratch1/sd/jialiu/fastpm/CorrelatedSims/wlen_jliu
 BIN=/global/cscratch1/sd/jialiu/fastpm/CorrelatedSims/wlen_jliu_multiple.py
 
+zs='{1}'
 srun python -u $BIN $DST $SRC $zs --zlmin 0.0 --zlmax 2.2 --zstep=0.2 --nside=4096
 
 #BIN=/global/cscratch1/sd/jialiu/fastpm/CorrelatedSims/wlen_jliu_1zs.py
@@ -38,10 +43,10 @@ srun python -u $BIN $DST $SRC $zs --zlmin 0.0 --zlmax 2.2 --zstep=0.2 --nside=40
 #srun python -u $BIN $DST $SRC $zs --zlmin 0.0 --zlmax 2.2 --zstep=0.02 --nside=4096 &
 #wait
 #done
-'''.format(zs)
+'''.format(zs, zs_str)
     f.write(content)
 
-for zs in np.arange(1.2, 1.4, 0.01):#(0.1, 0.2, 0.01):
+for zs in np.arange(1.2, 1.3, 0.01):#(0.1, 0.2, 0.01):
     write_sbatch(zs)
 
 
